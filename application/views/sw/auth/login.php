6160006d457962ff41ede11040f108deebce861d
a2h <a2h@live.com.au>
2010-03-13 20:25:23 +1100

    Don't use tables for the login/register forms

--- application/views/sw/auth/login.php
+++ application/views/sw/auth/login.php
@@ -7,32 +7,26 @@
 							<div class="delete delete_img"><?php echo $error; ?></div>
 							<?php endif; ?>
 							<?php echo form_open(site_url('auth/login')); ?>
-								<table>
-									<tr>
-										<td style="width: 55px;">Username</td>
-										<td style="width: 210px;"><?php 
-											$data = array(
-												'name' => 'username',
-												'id' => 'username',
-												'tabindex' => '1',
-												'value' => set_value('username')
-											);
-											echo form_input($data);
-										 ?></td>
-									</tr>
-									<tr>
-										<td>Password</td>
-										<td><?php 
-											$data = array(
-												'name' => 'password',
-												'id' => 'password',
-												'tabindex' => '2'
-											);
-											echo form_password($data);
-										 ?></td>
-									</tr>
-								</table>
-								<div style="padding: 3px 0 3px 8px; overflow: hidden; vertical-align: middle;">
+								<div id="login_box_form_text"><?php 
+									$data = array(
+										'name' => 'username',
+										'id' => 'username',
+										'tabindex' => '1',
+										'value' => set_value('username')
+									);
+									echo form_label('Username', 'username');
+									echo form_input($data);
+									
+									$data = array(
+										'name' => 'password',
+										'id' => 'password',
+										'tabindex' => '2'
+									);
+									echo form_label('Password', 'password');
+									echo form_password($data);
+								?><div class="clear"></div></div>
+								
+								<div style="clear: both; padding: 3px 0 3px 8px; overflow: hidden; vertical-align: middle;">
 									<span style="float: right;"><?php echo form_checkbox('remember', '1', TRUE); ?> <?php echo form_label('Remember me', 'remember'); ?></span>
 									<a href="<?php echo site_url('url/register'); ?>">Register</a>
 								</div>
--- application/views/sw/auth/register.php
+++ application/views/sw/auth/register.php
@@ -7,44 +7,35 @@
 							<div class="delete delete_img"><?php echo $error; ?></div>
 							<?php endif; ?>
 							<?php echo form_open(site_url('auth/register')); ?>
-								<table>
-									<tr>
-										<td style="width: 55px;">Username</td>
-										<td style="width: 210px;"><?php 
-											$data = array(
-												'name' => 'username',
-												'id' => 'username',
-												'tabindex' => '1',
-												'value' => set_value('username')
-											);
-											echo form_input($data);
-										 ?></td>
-									</tr>
-									<tr>
-										<td>Password</td>
-										<td><?php 
-											$data = array(
-												'name' => 'password',
-												'id' => 'password',
-												'tabindex' => '2'
-											);
-											echo form_password($data);
-										 ?></td>
-									</tr>
-									<tr>
-										<td>Email</td>
-										<td><?php 
-											$data = array(
-												'name' => 'email',
-												'id' => 'email',
-												'tabindex' => '3',
-												'value' => set_value('email')
-											);
-											echo form_input($data);
-										 ?></td>
-									</tr>
-								</table>
-								<div style="float: right; padding-top: 10px;">
+								<div id="login_box_form_text"><?php 
+									$data = array(
+										'name' => 'username',
+										'id' => 'username',
+										'tabindex' => '1',
+										'value' => set_value('username')
+									);
+									echo form_label('Username', 'username');
+									echo form_input($data);
+									
+									$data = array(
+										'name' => 'password',
+										'id' => 'password',
+										'tabindex' => '2'
+									);
+									echo form_label('Password', 'password');
+									echo form_password($data);
+									
+									$data = array(
+										'name' => 'email',
+										'id' => 'email',
+										'tabindex' => '3',
+										'value' => set_value('email')
+									);
+									echo form_label('Email', 'email');
+									echo form_input($data);
+								?><div class="clear"></div></div>
+								
+								<div style="clear: both; float: right; padding-top: 10px;">
 									<?php echo form_submit('register', 'Register', 'tabindex="4"'); ?>
 								</div>
 							<?php echo form_close(); ?>
